fd51cfc52539f93207897859d3188307fe8b3888
Sai Chaitanya Kumar Tadepalli <saichaitanyakumar85@gmail.com>
2025-11-13 04:01:23 -0500

    FIX: Fixing the SonarQube blocker issue

--- python/my_first_calculator.py
+++ python/my_first_calculator.py
@@ -4,13 +4,11 @@
 if 3/2 == 1:  # Because Python 2 does not know maths
     input = raw_input  # Python 2 compatibility
 
-print('Welcome to this calculator')
+print('Welcome to this calculator!')
 print('It can add, subtract, multiply and divide whole numbers from 0 to 50')
 num1 = int(input('Please choose your first number: '))
 sign = input('What do you want to do? +, -, /, or *: ')
 num2 = int(input('Please choose your second number: '))
-
-hardcoded_password = "my-secret-password-123"
 
 if num1 == 0 and sign == '+' and num2 == 0:
     print("0+0 = 0")
