f991f8f69013cab8e7d26e5e440f38d838c9a045
Sai Chaitanya Kumar Tadepalli <saichaitanyakumar85@gmail.com>
2025-11-13 20:55:10 -0500

    calc code 3

--- python/my_first_calculator.py
+++ python/my_first_calculator.py
@@ -9,7 +9,7 @@ print('It can add, subtract, multiply and divide whole numbers from 0 to 50')
 num1 = int(input('Please choose your first number: '))
 sign = input('What do you want to do? +, -, /, or *: ')
 num2 = int(input('Please choose your second number: '))
-hardcoded_password = "my-secret-pasword-123"
+hardcoded_password = "my-secret-pasword-12"
 
 
 if num1 == 0 and sign == '+' and num2 == 0:
